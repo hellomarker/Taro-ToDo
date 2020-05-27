5a00c81afe7c220c5c656664e7f2de75350332b3
yangt <1042164333@qq.com>
2020-05-27 18:16:10 +0800

    编辑readme.md文件

--- client/src/pages/index/matchDate.ts
+++ client/src/pages/index/matchDate.ts
@@ -1,6 +1,7 @@
-const regex = /([前昨今明后][天日早中晚])?([凌早中晚][晨午上])?[\d]((?<=0|1)[\d]|(?<=2)[0-4])?[点时:-]([\d]((?<=0|1)[\d]|(?<=2)[0-4])?[分])?/;
+const reg = /([前昨今明后][天日早中晚])?([凌早中晚][晨午上])?[\d]([\d]|(?<=2)[0-4])?[点时:-]([\d]([\d]|(?<=2)[0-4])?[分])?/;
 
 export function matchDate(str) {
-  let result = regex.exec(str);
+    debugger
+  let result = reg.exec(str);
   return result ? result[0] : undefined;
 }
